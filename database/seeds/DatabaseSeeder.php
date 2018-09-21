acdfa0a4660ddf1082555755f3ab1d13f26e819e
tugudush <jerome2kph@gmail.com>
2018-09-21 13:48:55 +0800

    simple db seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,16 +1,29 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Task;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        // $this->call(UsersTableSeeder::class);
-    }
-}
+  /**
+   * Run the database seeds.
+   *
+   * @return void
+   */
+
+  public function run() {
+    $this->call(TaskTableSeeder::class);
+  } // End of public function run()
+  
+} // End of class DatabaseSeeder extends Seeder
+
+class TaskTableSeeder extends seeder
+{
+  public function run() {
+    Task::truncate();
+    Task::create([
+      'body' => 'Wash dishes.',
+      'completed' => 0
+    ]);
+  } // End of public function run()
+} // End of class TaskTableSeeder extends seeeder
