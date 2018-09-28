5df5fbe7a4c8737e26029cc49d93bf8a96317ce8
tugudush <jerome2kph@gmail.com>
2018-09-28 15:01:04 +0800

    progress

---
+++ resources/views/layout/styles.blade.php
@@ -0,0 +1,7 @@
+<link href="{{ URL::asset('bower_components/bootstrap/dist/css/bootstrap.min.css') }}" rel="stylesheet">
+<link href="{{ URL::asset('bower_components/components-font-awesome/css/fontawesome-all.min.css') }}" rel="stylesheet">
+
+<link href="{{ URL::asset('css/presets.css') }}" rel="stylesheet">
+<link href="{{ URL::asset('css/utilities.css') }}" rel="stylesheet">
+<link href="{{ URL::asset('css/style.css') }}" rel="stylesheet">
+<link href="{{ URL::asset('css/media.css') }}" rel="stylesheet">
